d7ad9c44671bdd6c10396cf191256740e68d6e8a
Proladon <Proladon@gmail.com>
2021-05-13 20:12:02 +0800

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
+    <NavBar />
     <router-view/>
-    
   </div>
 </template>
 
 <script>
-
+import NavBar from '@/components/NavBar'
 export default {
   name: 'App',
+  components: {NavBar},
 
   data: () => ({
     //
