f47885ea51958d2305016c66d9298660aa09ab9c
Proladon <proladon@gmail.com>
2021-05-18 18:42:21 +0800

    api instance

--- src/api/file.js
+++ src/api/file.js
@@ -1,14 +1,17 @@
-import axios from 'axios';
-import configuration from '@/configuration';
+import { api } from '../api/instance'
 
 export const upload = async (blob) => {
   var formData = new FormData();
   formData.append("file", blob, 'xxx.pdf');
   
-  const res = await axios.post(`${configuration('backendHost')}/api/pdf`, formData, {
+  const res = await api ({
+    method: 'post',
+    url: '/pdf',
+    data: formData,
     headers: {
       'Content-Type': 'multipart/form-data'
     }
   })
+  
   return res.data
 }
