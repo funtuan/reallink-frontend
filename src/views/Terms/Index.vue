eaa40c305c4139c6ac351c1e440352954984503a
funtuan <hank.chen@ohbottech.com>
2021-05-19 14:42:20 +0800

    Fix IOS sms

--- src/views/Terms/Index.vue
+++ src/views/Terms/Index.vue
@@ -86,19 +86,27 @@ export default {
     // let unSupport = ls.get('unSupport')
 
     // if(!unSupport){
-
-    try {
-      const data = await this.$confirm('是否使用簡訊進行實聯制？')
-      console.log('data', data)
-      if (data === 'confirm') {
-        /* setTimeout(() => { 
-          alert('抱歉，此裝置不支援此功能，請使用台灣加密型實聯制')
-          // ls.set('unSupport', true)
-        }, 500); */
-        window.location = 'sms://1922?&body=場所代碼：111121314151617 12345123 本次實聯簡訊限防疫目的使用。'
+    const useAndroid = navigator.userAgent.match(/Android/i)
+    const useIOS = navigator.userAgent.match(/iPhone/i)
+    if (useAndroid || useIOS) {
+      try {
+        const data = await this.$confirm('是否使用簡訊進行實聯制？')
+        console.log('data', data)
+        if (data === 'confirm') {
+          /* setTimeout(() => { 
+            alert('抱歉，此裝置不支援此功能，請使用台灣加密型實聯制')
+            // ls.set('unSupport', true)
+          }, 500); */
+          if (useAndroid) {
+          window.location = 'sms://1922?&body=場所代碼：111121314151617%2012345123%20本次實聯簡訊限防疫目的使用。'
+          }
+          if (useIOS) {
+          window.location = 'sms:1922&body=場所代碼：111121314151617%2012345123%20本次實聯簡訊限防疫目的使用。'
+          }
+        }
+      } catch (error) {
+        console.log(error)
       }
-    } catch (error) {
-      console.log(error)
     }
     // }
 
